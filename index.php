ceaaec9d43ee2ec0415259c3317b732ddd289609
dwarf133 <mihail_danilchuk@mail.ru>
2020-12-22 18:18:56 +0500

    21.12 task 4 from homework 18.12

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?
-$name = 'Михаил';
-$age = 19;
+$time = date('H');
+if ($time >= 8 && time < 20)$img = 'morning_kitty.jpg';
+else if ($time < 8 || $time >= 20) $img = 'sleeping_kitty.jpg';
 ?>
 <!doctype html>
 <html lang="en">
@@ -9,13 +10,17 @@ $age = 19;
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Kitty Time!</title>
 </head>
 <body>
-    <p>
+    <div>
         <?
-            echo nl2br("Меня зовут $name \nМне $age лет.");
+        echo "<img width='500px' height='350px' src='./images/$img'>";
+        echo "<br>";
+        echo "У котят время: $time часов";
         ?>
-    </p>
+
+    </div>
+
 </body>
 </html>
