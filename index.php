28ec72eab7c882bdb23fdb7d1715881a910da4dc
dwarf133 <mihail_danilchuk@mail.ru>
2021-01-11 00:48:55 +0500

    smt class stuff

--- index.php
+++ index.php
@@ -1,21 +1,14 @@
 <?
-$name = 'Михаил';
-$age = '19';
+if(empty($_GET['id'])){
+    $id = 1;
+}
+else{
+    $id = (int)$_GET['id'];
+}
+if($id>3 || $id<1){
+    $id = 1;
+}
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <p>
-        <?
-            echo nl2br("Меня зовут $name \nМне $age лет.");
-        ?>
-    </p>
-</body>
-</html>
+<img width="450px" height="200px" src="./img/<?echo $id?>.jpg">
+<a href="index.php?id=<?echo ($id>1) ? $id-1 : 3; ?>">Prev</a>
+<a href="index.php?id=<?echo ($id<3) ? $id+1 : 1; ?>">Next</a>
