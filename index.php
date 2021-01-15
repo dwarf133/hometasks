2cfeda1b26e1bff8a67251845d5d82f0c243ce1a
dwarf133 <mihail_danilchuk@mail.ru>
2021-01-15 21:42:34 +0500

    reading colors from css

--- index.php
+++ index.php
@@ -1,10 +1,27 @@
 <?
-$str = fopen('read.txt', 'r');
-$inp = fread($str, 1);
-while ($inp != null){
-    if($inp == '\n')echo " ";
-    else if ($inp == 'o')echo "O";
-    else echo $inp;
+$str = file("./style/style.css");
+foreach ($str as $value) {
+    $arr= str_split($value, 1);
+    for($i=0; $i<count($arr); $i++){
+        if($arr[$i] == '#'){
+            $ans[] = $arr[$i] . $arr[$i+1] .$arr[$i+2] .$arr[$i+3] .$arr[$i+4] .$arr[$i+5] .$arr[$i+6];
+        }
+    }
 }
-echo "<br>";
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <select>
+        <option style="background-color: <?echo "$ans[0]";?>;">Цвет из style.css</option>
+        <option style="background-color: <?echo "$ans[1]";?>;">Цвет из style.css</option>
+    </select>
+</body>
+</html>
