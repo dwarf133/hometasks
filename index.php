83a3d19a41fd557460e68bdda2aaf38176203bb9
dwarf133 <mihail_danilchuk@mail.ru>
2021-01-15 19:58:50 +0500

    test

--- index.php
+++ index.php
@@ -1,32 +1,10 @@
 <?
-// задание 1
-session_start();
-session_destroy();
-
-//задание 2
-if(isset($_POST['color'])){
-    setcookie("color", $_POST['color']);
-    $color = $_POST['color'];
+$str = fopen('read.txt', 'r');
+$inp = fread($str, 1);
+while ($inp != null){
+    if($inp == '\n')echo " ";
+    else if ($inp == 'o')echo "O";
+    else echo $inp;
 }
-else if(isset($_COOKIE['color'])){
-    $color = $_COOKIE['color'];
-}
+echo "<br>";
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body style="background-color: <?echo $color;?>;">
-    <form method="post">
-        Красный<input name="color" value="red" type="radio"><br>
-        Синий<input name="color" value="blue" type="radio"><br>
-        Желтый<input name="color" value="yellow" type="radio"><br>
-        <input type="submit"><br>
-    </form>
-</body>
-</html>
--- task1.php
+++
@@ -1,8 +0,0 @@
-<?
-session_start();
-?>
-Сколько будет 2+2?<br>
-<form method="post" action="task2.php">
-    <input name="ans1" type="text">
-    <input type="submit">
-</form>
