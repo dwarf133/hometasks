125cfe719294cb345f994447c828d0045401f401
dwarf133 <mihail_danilchuk@mail.ru>
2021-01-22 01:00:47 +0500

    classwork so on

--- index.php
+++ index.php
@@ -1,30 +1,29 @@
-<?
-$str = file("./style/style.css");
-foreach ($str as $value) {
-    $arr= str_split($value, 1);
-    for($i=0; $i<count($arr); $i++){
-        if($arr[$i] == '#'){
-            $ans[] = $arr[$i] . $arr[$i+1] .$arr[$i+2] .$arr[$i+3] .$arr[$i+4] .$arr[$i+5] .$arr[$i+6];
-        }
+<?php
+$hostname = "localhost";
+$username = "dwarf133";
+$password = "qazwsxEDC09";
+$dbname = "people";
+
+/*$db_con = mysqli_connect($hostname, $username, $password, $dbname);
+//var_dump($db_con);
+
+mysqli_set_charset($db_con, 'utf8');
+
+$sql = mysqli_query($db_con, "select * from name");
+$arr = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+echo "<pre>";
+print_r($arr);
+echo "</pre>";
+foreach ( $arr as $item){
+    foreach ($item as $key => $value){
+        if($key == "name")echo "$value ";
     }
-}
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <select>
-        <?
-        foreach ($ans as $temp){
-            echo "<option style='background-color: $temp;'>Цвет из style.css</option>";
-        }
-        ?>
-    </select>
-</body>
-</html>
+}*/
+
+$db_con = mysqli_connect($hostname, $username, $password, $dbname);
+mysqli_query($db_con, "CREATE TABLE People (
+    id_pearson int(5) auto_increment primary key,
+    name varchar(20) not null,
+    surname varchar(40) not null,
+    age int  
+)");
